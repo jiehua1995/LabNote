7b57547429d117a21232c046ff543efa99ceb413
jiehua1995 <fdjiehua@gmail.com>
2023-09-28 00:18:52 +0200

    0.0.1

--- GO/go.mod
+++ GO/go.mod
@@ -1,3 +1,5 @@
 module LabNote_GO
 
 go 1.13
+
+require github.com/akavel/rsrc v0.10.2 // indirect
--- GO/main.go
+++ GO/main.go
@@ -3,18 +3,58 @@ package main
 import (
     "fmt"
     "os"
+    "time"
     "LabNote_GO/utils"
 )
 
 func main() {
+    // date selection
     selectedDate := utils.GetDate()
     formated_selectedDate := selectedDate.Format("2006-01-02")
     fmt.Printf("Selected date is: %s\n", selectedDate.Format("2006-01-02"))
-    fileType := utils.GetCategory()
-    fmt.Printf("Selected category is: %s\n", fileType)
+
+    // category selection
+    fmt.Println("=================\nPlease select one category (1-4):")
+	fmt.Println("1. Experimental log")
+	fmt.Println("2. Discussion")
+	fmt.Println("3. Reading")
+	fmt.Println("4. Inspiration\n=================")
+
+	var choice int
+	_, err := fmt.Scanf("%d", &choice)
+	if err != nil {
+		fmt.Println("Inavilable selection:", err)
+		os.Exit(1)
+	}
+
+	var category string
+    var content string
+	switch choice {
+	case 1:
+		category = "Experimental log"
+		content = "## Experiments\n\n"
+	case 2:
+		category = "Discussion"
+        content = "## Content\n\n"
+	case 3:
+		category = "Reading"
+        content = "**Title:**\n\n**DOI:**\n\n**URL:**\n\n**Published:**\n\n**Journal:**\n\n##Notes\n\n"
+	case 4:
+		category = "Inspiration"
+        content = "## Content\n\n"
+	default:
+		fmt.Println("Inavilable input, please select from 1 to 4。")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Selected category is: %s\n", category)
+
+	yaml := "---\nDate: " + formated_selectedDate + "\nAuthor: Jie Hua\nEmail: Jie.Hua@lmu.de\nCategory: "+ category + "\n---\n\n[toc]\n\n## Basic Information\n\n"
+
+	MdContent := yaml + content
 
     // merge file name
-    filename := fileType + "_" + formated_selectedDate + ".md"
+    filename := category + "_" + formated_selectedDate + ".md"
     // create markdown file
     file, err := os.Create(filename)
     if err != nil {
@@ -24,7 +64,7 @@ func main() {
     defer file.Close()
     fmt.Printf("File created: %s\n", filename)
 
-    // write yaml information
+    // write markdown information
     file, err = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
     if err != nil {
         fmt.Println("Could not open the file: ", err)
@@ -32,12 +72,17 @@ func main() {
     }
     defer file.Close()
 
-    yaml := "---\nDate: " + formated_selectedDate + "\nauthor: Jie Hua\nemail: Jie.Hua@lmu.de\ncategory: "+ fileType + "\n---"
-    _, err = file.WriteString(yaml)
+
+    _, err = file.WriteString(MdContent)
     if err != nil {
         fmt.Println("Error in writing into the file:", err)
         return
     }
-    fmt.Println("Basic information was written successfully")
-    fmt.Println("\rPress Enter to exit...")
+    fmt.Println("Markdown file generated successfully")
+
+    fmt.Println("Software will be closed after ")
+    fmt.Println("2 seconds...............")
+    time.Sleep(1 * time.Second)
+    fmt.Println("1 second..........")
+    time.Sleep(1 * time.Second)
 }
