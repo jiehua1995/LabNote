2be59d4aaa68ec31d4a7b7f2902cce99c7c1ff37
jiehua1995 <fdjiehua@gmail.com>
2024-01-09 15:30:10 +0100

    gui

--- GUI/electron/main.js
+++ GUI/electron/main.js
@@ -6,6 +6,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "favicon.ico", // Update the path
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
